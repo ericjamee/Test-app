432a26a5518103aa2c413873e4ac7e2b410c84c4
James Erickson <je7erickson@gmail.com>
2025-03-07 15:44:40 -0700

    Initial Commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,47 @@
+import './App.css'
+
+  const bandNames = [
+    {name: "Dire Straits", members: "Mark, David, John, Pick", formed: 1977},
+    {name: "R.E.M.", members: "Michael, Peter, Mike, Bill", formed: 1980},
+    {name: "Collective Soul", members: "Ed, Dean, David, Ross, Shane", formed: 1992}
+  ];
+
+
+function Welcome() {
+  return <h1>Criminally Underrated Bands</h1>
+}
+
+function Band({name, members, formed}: {name: string; members: string, formed: number}) {
+  return(
+    <>
+    <img />
+    <h2>{name}</h2>
+    <h3>Original Members: {members}</h3>
+    <h3>Formed: {formed}</h3>
+    </>
+  )
+}
+
+function BandList() {
+  return(
+    <>
+    {
+      bandNames.map((singleBand)=> (
+        <Band {...singleBand}/>
+      ))
+    }
+    </>
+  )
+}
+
+function App() {
+
+  return (
+    <>
+      <Welcome />
+      <BandList /> 
+    </>
+  )
+}
+
+export default App
